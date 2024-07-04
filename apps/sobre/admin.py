bf85963e11b3413892e77d5080fbb037bd3283e3
Júlio Soares <juliocsoaresa1@gmail.com>
2024-07-04 15:01:46 -0300

    feat: criando modelos de valores e nossos valores

--- apps/sobre/admin.py
+++ apps/sobre/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Intro
+from .models import Intro, NossosValores, Valores
 
 
 class IntroAdmin(admin.ModelAdmin):
@@ -10,4 +10,22 @@ class IntroAdmin(admin.ModelAdmin):
     fieldsets = ()
 
 
+class NossosValoresAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo',)
+    list_display_links = ('id', 'titulo',)
+    list_filter = ('titulo',)
+    filter_horizontal = ()
+    fieldsets = ()
+
+
+class ValoresAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo',)
+    list_display_links = ('id', 'titulo',)
+    list_filter = ('titulo',)
+    filter_horizontal = ()
+    fieldsets = ()
+
+
 admin.site.register(Intro, IntroAdmin)
+admin.site.register(NossosValores, NossosValoresAdmin)
+admin.site.register(Valores, ValoresAdmin)
